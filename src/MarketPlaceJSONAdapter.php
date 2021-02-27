2f1e5001f8c1f4a560f79ae9cde69be6ec5c25f0
Rodrigo Soares <rodrigosoares8899@gmail.com>
2021-02-27 17:19:49 -0300

    Finish marketplace Json Adapter

--- src/MarketPlaceJSONAdapter.php
+++ src/MarketPlaceJSONAdapter.php
@@ -4,11 +4,39 @@
 namespace Src;
 
 
+use Lib\MarketplaceJSON\Handler;
+use Lib\MarketplaceJSON\Order;
+
 class MarketPlaceJSONAdapter implements Updateable
 {
+    public Handler $handler;
+
+    public string $json;
+
+    public function __construct(Handler $handler, string $json)
+    {
+        $this->handler = $handler;
+        $this->json = $json;
+    }
 
     public function updateOrder()
     {
-        return false;
+        $order = $this->parseOrder();
+
+        return $this->handler->updateOrder($order);
+    }
+
+    public function parseOrder() : Order
+    {
+        $object = json_decode($this->json);
+
+        $order = new Order(
+            $object->order->client->name,
+            $object->order->seller->name,
+            $object->order->value,
+            $object->order->discountValue
+        );
+
+        return $order;
     }
 }
